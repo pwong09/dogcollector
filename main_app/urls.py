61305d84e5f85e820e6fa37d0abd639548be05a3
pwong09 <pwong09@gmail.com>
2022-04-22 16:42:22 -0700

    set up for adding a new dog

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('dogs/', views.dogs_index, name='index'),
+    path('dogs/add', views.add_dogs, name='add'),
     path('<int:dog_id>/', views.dogs_detail, name='detail'),
+    path('dogs/new', views.new_dog, name='new')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -21,3 +21,12 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = get_object_or_404(Dog, pk=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+def add_dogs(request):
+    return render(request, 'dogs/add.html')
+
+def new_dog(request):
+    print(request.body)
+#   new = Dog.objects.create(name=, breed=, description=, age=)
+#   new.save()
+#   return HTTPResponseRedirect('dogs/index.html')
