e054ae940a273a11bcc3804588e608b649506836
pwong09 <pwong09@gmail.com>
2022-04-25 11:47:30 -0700

    edit details page

--- main_app/views.py
+++ main_app/views.py
@@ -12,9 +12,9 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
-    dog_list = Dog.objects.all()
+    dogs = Dog.objects.all()
     context = {
-        'dog_list': dog_list
+        'dogs': dogs
     }
     return render(request, 'dogs/index.html', context)
 
