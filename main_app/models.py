301b5d474ed3734b548cb5c68ac07572eeea0f6d
pwong09 <pwong09@gmail.com>
2022-04-26 14:47:29 -0700

    establish m:m relationship between dog:toy

--- main_app/models.py
+++ main_app/models.py
@@ -10,37 +10,6 @@ MEALS = (
     ('D', 'Dinner')
 )
 
-# Create your models here.
-class Dog(models.Model):
-    name = models.CharField(max_length=75)
-    breed = models.CharField(max_length=150)
-    description = models.CharField(max_length=255)
-    age = models.DecimalField(max_digits = 4, decimal_places = 1)
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'dog_id': self.id})
-    
-    def fed_for_today(self):
-        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
-
-class Feeding(models.Model):
-    date = models.DateField('feeding date')
-    meal = models.CharField(
-        max_length=1,
-        choices=MEALS,
-        default=MEALS[0][0]
-        )
-    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.get_meal_display()} on {self.date}"
-    
-    class Meta:
-        ordering = ['-date']
-
 class Toy(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
@@ -65,3 +34,39 @@ class Toy(models.Model):
     
     def get_absolute_url(self):
         return reverse('toy_detail', kwargs={'pk': self.id})
+
+# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=75)
+    breed = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+    age = models.DecimalField(max_digits = 4, decimal_places = 1)
+    toys = models.ManyToManyField(Toy)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+    class Meta:
+        ordering = ['name']
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+        )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
+
--- main_app/views.py
+++ main_app/views.py
@@ -36,9 +36,11 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     feeding_form = FeedingForm()
+    available_toys = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
     return render(request, 'dogs/detail.html', {
         'dog': dog,
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': available_toys
         })
 
 def add_feeding(request, dog_id):
@@ -66,3 +68,7 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys/'
+
+def assoc_toy(request, dog_id, toy_id):
+    Dog.objects.get(id=dog_id).toys.add(toy_id)
+    return redirect('detail', dog_id=dog_id)
